ef4cf9837021e4e468300b08fed6ef8d140913e5
JHON PIER <jhonpierta@gmail.com>
2024-01-11 20:49:39 -0500

    Cambios

---
+++ Stocks.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Stocks.Domain.Repositories;
+using Stocks.Infrastructure.Repositories;
+using MongoDB.Driver;
+
+
+namespace Stocks.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static void AddInfraestructure(
+            this IServiceCollection services, string connectionString
+            )
+        {
+
+            services.AddDataBaseFactories(connectionString);
+            services.AddRepositories();
+        }
+
+        private static void AddDataBaseFactories(this IServiceCollection services, string connectionString)
+        {            
+            services.AddSingleton(mongoDatabase =>
+            {
+                var mongoClient = new MongoClient(connectionString);
+                return mongoClient.GetDatabase("db-productos-stocks");
+            });
+
+        }
+
+        private static void AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IProductoRepository, ProductoRepository>();
+
+            
+        }
+    }
+}
